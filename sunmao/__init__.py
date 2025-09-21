4b8fe99f82f05ec158312b414578b471a6c901e2
seqyuan <ahworld07@gmail.com>
2025-09-21 23:48:27 +0800

    v0.3.2

--- sunmao/__init__.py
+++ sunmao/__init__.py
@@ -3,8 +3,9 @@ Sunmao: A flexible subplot layout library for matplotlib.
 
 Sunmao provides a flexible way to create complex subplot layouts using the
 traditional Chinese mortise-tenon joinery concept. Each mortise can have
-child tenons in all directions (up, down, left, right) around a central mortise.
-Each mortise is an independent matplotlib axes object that can be fully customized.
+child tenons in all directions (up, down, left, right) around a central
+mortise. Each mortise is an independent matplotlib axes object that can be
+fully customized.
 
 Main classes:
 - mortise: The main building block for creating layouts
@@ -14,6 +15,5 @@ Main classes:
 
 from .mortise_tenson import mortise, LegendManager, LegendPosition
 
-__version__ = "0.3.0"
+__version__ = "0.3.2"
 __all__ = ["mortise", "LegendManager", "LegendPosition"]
-
